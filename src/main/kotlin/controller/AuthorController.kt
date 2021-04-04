8bc7163e2695e07b8e4dbd0b077f516621a06b09
Ian Oliveira <ianprogrammer@gmail.com>
2021-04-04 01:54:53 -0300

    refactor: add some response

--- src/main/kotlin/controller/AuthorController.kt
+++ src/main/kotlin/controller/AuthorController.kt
@@ -1,9 +1,9 @@
 package com.ian.deveficiente.controller
 
+import com.fasterxml.jackson.core.JsonParseException
 import com.ian.deveficiente.dto.AuthorDTO
-import com.ian.deveficiente.dto.toAuthorDomain
 import com.ian.deveficiente.service.contract.AuthorRepository
-import com.ian.deveficiente.service.impl.AuthorRepositoryImpl
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
@@ -11,6 +11,7 @@ import javax.inject.Inject
 import javax.validation.Valid
 import io.micronaut.http.HttpResponse;
 import io.micronaut.validation.Validated
+
 
 @Controller("/\${api.version:v1}/author")
 @Validated
@@ -21,10 +22,10 @@ class AuthorController(
 
 
     @Post
-    fun addController(@Body @Valid  data: AuthorDTO): HttpResponse<*> {
+    fun addController(@Body @Valid data: AuthorDTO): HttpResponse<String> {
         return try {
             authorRepository.save(data)
-            HttpResponse.ok("")
+            HttpResponse.ok("created")
         } catch (ex: Exception) {
             HttpResponse.badRequest(ex.message)
         }
